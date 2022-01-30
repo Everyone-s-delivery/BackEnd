21b98d9d257f02d9f9a64eb3f31663602b7d97a9
rnjstjdgh <gshgsh0831@daum.net>
2022-01-30 18:02:20 +0900

    S

--- src/main/java/everyone/delivery/demo/domain/post/PostController.java
+++ src/main/java/everyone/delivery/demo/domain/post/PostController.java
@@ -24,50 +24,50 @@ public class PostController {
 
     private final PostService postService;
 
-//    @GetMapping("")
-//    @ApiOperation(value = "글 리스트 조회", notes = "글 리스트를 조회할 수 있습니다.")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
-//    })
-//    public ResponseEntity getById(){
-//        return ResponseUtils.out(postService.getList());
-//    }
-//
-//    @GetMapping("{postId}")
-//    @ApiOperation(value = "글 개별 조회", notes = "postId에 해당하는 글을 조회할 수 있습니다.")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
-//    })
-//    public ResponseEntity getById(@PathVariable @Min(value = 1, message = "postId cannot be minus.")Long postId){
-//        return ResponseUtils.out(postService.getById(postId));
-//    }
-//
-//    @PostMapping("")
-//    @ApiOperation(value = "글 등록", notes = "모집 글을 등록한다.")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
-//    })
-//    public ResponseEntity create(@Valid @RequestBody @ApiParam(value = "모집 글 정보를 갖는 객체", required = true) CreatePostDto createPostDto){
-//        return ResponseUtils.out(postService.create(createPostDto));
-//    }
-//
-//    @PutMapping("{postId}")
-//    @ApiOperation(value = "글 수정", notes = "모집 글을 수정한다.")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
-//    })
-//    public ResponseEntity update(@PathVariable @Min(value = 1, message = "postId cannot be minus.")Long postId,
-//                                 @Valid @RequestBody @ApiParam(value = "모집 글의 수정 정보를 갖는 객체", required = true) UpdatePostDto updatePostDto){
-//        return ResponseUtils.out(postService.update(postId,updatePostDto));
-//    }
-//
-//    @DeleteMapping("/{postId}")
-//    @ApiOperation(value = "기존 모집 글 삭제",
-//            notes = "기존 덧글을 삭제합니다.")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
-//    })
-//    public ResponseEntity delete(@PathVariable @Min(value = 1, message = "postId cannot be minus.") Long postId) {
-//        return ResponseUtils.out(postService.delete(postId));
-//    }
+    @GetMapping("")
+    @ApiOperation(value = "글 리스트 조회", notes = "글 리스트를 조회할 수 있습니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity getById(){
+        return ResponseUtils.out(postService.getList());
+    }
+
+    @GetMapping("{postId}")
+    @ApiOperation(value = "글 개별 조회", notes = "postId에 해당하는 글을 조회할 수 있습니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity getById(@PathVariable @Min(value = 1, message = "postId cannot be minus.")Long postId){
+        return ResponseUtils.out(postService.getById(postId));
+    }
+
+    @PostMapping("")
+    @ApiOperation(value = "글 등록", notes = "모집 글을 등록한다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity create(@Valid @RequestBody @ApiParam(value = "모집 글 정보를 갖는 객체", required = true) CreatePostDto createPostDto){
+        return ResponseUtils.out(postService.create(createPostDto));
+    }
+
+    @PutMapping("{postId}")
+    @ApiOperation(value = "글 수정", notes = "모집 글을 수정한다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity update(@PathVariable @Min(value = 1, message = "postId cannot be minus.")Long postId,
+                                 @Valid @RequestBody @ApiParam(value = "모집 글의 수정 정보를 갖는 객체", required = true) UpdatePostDto updatePostDto){
+        return ResponseUtils.out(postService.update(postId,updatePostDto));
+    }
+
+    @DeleteMapping("/{postId}")
+    @ApiOperation(value = "기존 모집 글 삭제",
+            notes = "기존 덧글을 삭제합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token(사용자 토큰)", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity delete(@PathVariable @Min(value = 1, message = "postId cannot be minus.") Long postId) {
+        return ResponseUtils.out(postService.delete(postId));
+    }
 }
